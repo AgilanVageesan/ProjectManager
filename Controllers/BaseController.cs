a6ddb44a013cb3c0725a38109b4871c2786ea1ac
Agilan Vageesan <agilanvageesan@gmail.com>
2021-03-02 15:13:32 +0530

    feat(master): Sprint 1 Assignment

---
+++ Controllers/BaseController.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProjectManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController<T> : ControllerBase
+    {
+        private readonly ApiDbContext _context;
+
+        public BaseController(ApiDbContext context)
+        {
+            _context = context;
+        }
+        
+
+    }
+}
---
+++ Models/Project.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ProjectManager.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Detail { get; set; }
+        public DateTime CreatedOn { get; set; }
+
+    }
+}
